96720e5616713df64ab194ab2f23e31c6f4bd080
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-06-02 11:06:05 +0900

    Feat: GlobalStyle Settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { RouterInfo } from './util/router';
+import GlobalStyle from './util/GlobalStyle';
 
 const RouterObject = createBrowserRouter(RouterInfo);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={RouterObject} />
   </React.StrictMode>,
 );
