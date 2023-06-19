2dedadef36dc1190f97016d46a958bf36d564898
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-06-19 23:21:03 +0900

    Feat: 구단 선택 페이지 완성

--- src/pages/Matchmaking.tsx
+++ src/pages/Matchmaking.tsx
@@ -1,20 +1,12 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
+type props = {};
 
-const Matchmaking = () => {
-  return (
-    <DIV>
-      dfdsfsfsdfsdf
-    </DIV>
-  );
+const Matchmaking = (props: props) => {
+  return <DIV>dfdsfsfsdfsdf</DIV>;
 };
 
 export default Matchmaking;
 
-
-const DIV = styled.div`
-  width:1000px;
-  height:1000px;
-  background-color:red;
-`;
+const DIV = styled.div``;
