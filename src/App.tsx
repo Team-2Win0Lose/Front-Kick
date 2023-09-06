55967d314a6655ed70afa152f5314536cd4b2152
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-09-06 22:20:27 +0900

    Fix: 로딩창 원래대로 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { ScrollRestoration, Outlet, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { ScrollRestoration, Outlet } from 'react-router-dom';
 import Layout from './components/Layouts/Layout';
-import Loading from './pages/Loading/Loading';
 function App() {
-  const [loading, setloading] = useState(false);
-  const navigate = useNavigate();
-  return loading ? (
+  return (
     <Layout>
       <ScrollRestoration />
       <Outlet />
     </Layout>
-  ) : (
-    <div
-      onLoad={() =>
-        setTimeout(() => {
-          navigate('/home');
-          setloading(true);
-        }, 2000)
-      }
-    >
-      <Loading />
-    </div>
   );
 }
 
